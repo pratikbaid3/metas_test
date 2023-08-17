166444cfd9048e3b78f1fa021c1c6713715743b2
pratikbaid3 <pratikbaid3@gmail.com>
2023-08-18 02:42:25 +0530

    feat: Added tests files

---
+++ test/unit_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:metas_test/modules/dashboard/apis/character_provider.dart';
+import 'package:metas_test/modules/dashboard/models/character_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  testWidgets('fetches character list successfully',
+      (WidgetTester tester) async {
+    final charactersData = {
+      "RelatedTopics": [
+        {"name": "Character 1"},
+        {"name": "Character 2"},
+      ]
+    };
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider(
+          create: (context) =>
+              CharacterProvider(), // Create a simple HomeProvider instance
+          child: Builder(
+            builder: (context) => FloatingActionButton(
+              onPressed: () =>
+                  Provider.of<CharacterProvider>(context, listen: false)
+                      .getCharacterList(context),
+              child: const Text('Fetch Characters'),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Simulate tapping the button
+    await tester.tap(find.text('Fetch Characters'));
+    await tester.pumpAndSettle();
+
+    // Get the current instance of HomeProvider
+    final characterProvider = Provider.of<CharacterProvider>(
+        tester.element(find.byType(FloatingActionButton)));
+
+    expect(characterProvider.characters, isA<List<CharacterModel>>());
+    expect(characterProvider.characters.length, 2);
+    // Add more assertions as needed
+  });
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,39 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:metas_test/main_common.dart';
+import 'package:metas_test/modules/dashboard/apis/character_provider.dart';
+import 'package:metas_test/modules/dashboard/models/character_model.dart';
+import 'package:metas_test/modules/dashboard/widgets/character_detail_widget.dart';
+import 'package:metas_test/modules/dashboard/widgets/list_widget.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('renders character list correctly', (WidgetTester tester) async {
+    // Create a mock CharacterProvider with sample data
+    final characterProvider = CharacterProvider();
+    characterProvider.characters = [
+      CharacterModel(name: 'Character 1', image: 'image_url_1'),
+      CharacterModel(name: 'Character 2', image: 'image_url_2'),
+    ];
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Build the CharacterListWidget wrapped in a ChangeNotifierProvider
+    await tester.pumpWidget(
+      MaterialApp(
+        home: ChangeNotifierProvider.value(
+          value: characterProvider,
+          child: const CharacterListWidget(),
+        ),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify the presence of both characters in the list
+    expect(find.text('Character 1'), findsOneWidget);
+    expect(find.text('Character 2'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Tap on the first character
+    await tester.tap(find.text('Character 1'));
+    await tester.pumpAndSettle();
+
+    // Verify that the character detail widget is navigated to
+    expect(find.byType(CharacterDetailWidget), findsOneWidget);
   });
 }
